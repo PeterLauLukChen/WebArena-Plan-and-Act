b62becdf2fe7188666b6b3773a791ebd4ea017ae
Peter Lau-Luk Chen <anonymousneurips24@gmail.com>
2025-07-08 21:20:21 -0400

    Add files via upload

---
+++ agent/prompts/__init__.py
@@ -0,0 +1,23 @@
+from .prompt_constructor import (
+    PromptConstructor,
+    DirectPromptConstructor,
+    CoTPromptConstructor,
+    MultimodalCoTPromptConstructor,
+    WebRLPromptConstructor,
+    WebRLChatPromptConstructor,
+    SystemMessagePromptConstructor,
+    PlannerPromptConstructor,
+    ExecutorPromptConstructor
+)
+
+__all__ = [
+    "PromptConstructor",
+    "DirectPromptConstructor",
+    "CoTPromptConstructor",
+    "MultimodalCoTPromptConstructor",
+    "WebRLPromptConstructor",
+    "WebRLChatPromptConstructor",
+    "SystemMessagePromptConstructor",
+    "PlannerPromptConstructor",
+    "ExecutorPromptConstructor"
+]
